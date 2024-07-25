74ef54e2ea353c70baa972ee5ffcbe37054bc290
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-07-25 05:22:12 +0000

    iterable objects and magic methods

--- 6_Loops/loopsExplanation.py
+++ 6_Loops/loopsExplanation.py
@@ -1,2 +1,7 @@
-# Iterable objects
+# Iterable objects - lists,files
+# Iteration tools - for,while loop
+# no do-while loop in python
+# __next__()
+# __init__() and __str__() are magic methods and acts constructor intialization
+
 
