7dbf8bfe2be8cf1e76bb9562135962ca42120bee
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-07-26 05:30:56 +0000

    added pillars of oops

--- 11_OOP/oops.py
+++ 11_OOP/oops.py
@@ -2,8 +2,21 @@ class Car:
     def __init__(self,model,brand):
         self.brand=brand
         self.model=model
+    
+    def fullName(self):
+       return f"{self.model}{self.brand}"
+# Inheritance
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model)
+        self.battery_size=battery_size
+
+
 
 myCar=Car("abc","abd")
 print(Car)
 print(myCar)
 print(myCar.model , myCar.brand)
+print(myCar.fullName())
+myTesla=ElectricCar("tesla","abc","85kWH")
+print(myTesla.battery_size)
