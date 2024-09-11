eff3aad73c6900a865a510876076fe51105dcd49
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-09-11 06:40:14 +0000

    list , set, tuple concepts

--- DSA/basics.py
+++ DSA/basics.py
@@ -13,3 +13,20 @@ ans = fib(3)
 print(ans)
 
 #  Write a Python function to find common elements between two lists.
+
+# List: Ordered, mutable, allows duplicates. Use it when you need a collection where elements can be modified and order matters.
+# Set: Unordered, mutable, no duplicates. Use it when you need a collection of unique elements and don’t care about the order.
+# Tuple: Ordered, immutable, allows duplicates. Use it when you need a fixed collection of elements that should not be changed and need to maintain their order.
+
+
+def common(list1,list2):
+    set1=set(list1)
+    set2=set(list2)
+    return list(set1.intersection(set2))
+
+list1 = [1,2,3,4]
+list2 = [3,4,5]
+
+ans = common(list1, list2)
+
+print(ans)
