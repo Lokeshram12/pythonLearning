827185966876b417019a22a616f6d7fb2866ca00
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-11-17 10:58:40 +0000

    added a flask application

--- APIdevelopment/main.py
+++ APIdevelopment/main.py
@@ -1,1 +1,7 @@
-import requets
+import requests, json
+response = requests.get('https://api.stackexchange.com//2.3/answers?order=desc&sort=activity&site=stackoverflow')
+
+print(response)
+# print(response.json())
+for data in response.json()['items']:
+    print(data['owner'])
